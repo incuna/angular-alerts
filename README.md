b0e030879c3ac2e47abad960b2eae34cbdbdd5ab
Max Peterson <max@incuna.com>
2014-06-20 17:16:35 +0100

    initial version

---
+++ README.md
@@ -0,0 +1,8 @@
+# angular-alerts
+
+
+An alerts service and directive for setting and displaying alert messages. The alerts directive uses the Twitter Bootstrap Alert component.
+
+# Installation
+1. `bower install angular-alerts --save`
+1. Inject `angular-alerts` in to your app module.
---
+++ alerts.js
@@ -0,0 +1,83 @@
+
+(function (angular) {
+    'use strict';
+
+    var alerts = angular.module('alerts', []);
+
+    alerts.factory('AlertsFactory', ['$rootScope',
+        function ($rootScope) {
+            return function () {
+                var store = [];
+                var addListeners = [];
+                var clearListeners = [];
+                this.clear = function () {
+                    store = [];
+                    angular.forEach(clearListeners, function (listener) {
+                        listener.call(null);
+                    });
+                };
+                this.add = function (message, type) {
+                    if (angular.isUndefined(store)) {
+                        store = [];
+                    }
+                    var data = {
+                        message: message,
+                        type: type || 'info'
+                    };
+                    store.push(data);
+                    angular.forEach(addListeners, function (listener) {
+                        listener.call(null, data);
+                    });
+
+                };
+                this.get = function () {
+                    return store;
+                };
+
+                this.onAdd = function (listener) {
+                    addListeners.push(listener);
+
+                };
+
+                this.onClear = function (listener) {
+                    clearListeners.push(listener);
+                };
+                
+            };
+        }
+    ]);
+
+    alerts.directive('alerts', [
+        function () {
+            return {
+                restrict: 'A',
+                templateUrl: 'templates/alerts/alerts.html',
+                transclude: true,
+                scope: {
+                    alerts: '='
+                },
+                link: function (scope, element, attrs) {
+                    scope.$watch('alerts', function (alerts) {
+                        if (angular.isDefined(alerts)) {
+                            // Wire up the alerts
+                            scope.messages = scope.alerts.get();
+                            alerts.onAdd(function (message) {
+                                scope.messages.push(message);
+                            });
+                            alerts.onClear(function () {
+                                scope.messages = [];
+                            });
+                        }
+                    });
+                }
+            };
+        }]
+    );
+
+    alerts.run(['$templateCache', function ($templateCache) {
+        $templateCache.put('templates/alerts/alerts.html',
+            '<alert ng-repeat="item in messages" type="item.type" ng-bind="item.message"></alert>'
+        );
+    }]);
+
+}(window.angular));
